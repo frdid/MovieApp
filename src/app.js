036a55083b009956e8bcf30ceb0d256f3e040a15
Ferdi Dirgantara <ferdifordev@gmail.com>
2020-05-21 04:27:59 +0700

    create movie-item component

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ import 'simplebar';
 import 'simplebar/dist/simplebar.css';
 import "./styles/style.css";
 import "../src/js/component/app-bar.js";
+import "./js/component/movie-item.js";
+import "./js/component/user-card.js";
 import main from './js/view/main.js'
 
 document.addEventListener("DOMContentLoaded", main);
